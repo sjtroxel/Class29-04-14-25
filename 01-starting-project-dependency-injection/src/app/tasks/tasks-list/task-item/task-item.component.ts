b939d0bb19b6c08307d63d292ce320c7170e4998
Steffan Troxel <sjtroxel@alumni.iu.edu>
2025-04-13 19:44:26 -0500

    tokens and providers, dependency injection

--- 01-starting-project-dependency-injection/src/app/tasks/tasks-list/task-item/task-item.component.ts
+++ 01-starting-project-dependency-injection/src/app/tasks/tasks-list/task-item/task-item.component.ts
@@ -1,7 +1,9 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { Task, TaskStatus } from '../../task.model';
+import { Task, TASK_STATUS_OPTIONS, TaskStatus } from '../../task.model';
+// import { TasksService } from '../../tasks.service';
+import { TasksServiceToken } from '../../../../main';
 
 @Component({
   selector: 'app-task-item',
@@ -11,6 +13,8 @@ import { Task, TaskStatus } from '../../task.model';
   styleUrl: './task-item.component.css',
 })
 export class TaskItemComponent {
+  private tasksService = inject(TasksServiceToken);
+  taskStatusOptions = inject(TASK_STATUS_OPTIONS)
   task = input.required<Task>();
   taskStatus = computed(() => {
     switch (this.task().status) {
@@ -41,5 +45,7 @@ export class TaskItemComponent {
       default:
         break;
     }
+
+    this.tasksService.updateTaskStatus(taskId, newStatus);
   }
 }
